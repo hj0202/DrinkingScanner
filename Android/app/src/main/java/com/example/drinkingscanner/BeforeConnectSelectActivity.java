bd0e5a02045738ef541ad24222d9e66dca7d1712
nhee0410 <nhee0410@naver.com>
2021-09-04 13:33:35 +0900

    [Android] Feat : 서비스 선택 화면  구현
    서비스 선택(기기 연결/사용자 음주 습관 통계 중 선택)화면 구현 완료

---
+++ Android/app/src/main/java/com/example/drinkingscanner/BeforeConnectSelectActivity.java
@@ -0,0 +1,72 @@
+package com.example.drinkingscanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class BeforeConnectSelectActivity extends Activity {
+
+    private Button btnSelectSoju;
+    private Button btnSelectBeer;
+    private Button btnSelectWine;
+    private Button btnSelectMakkulli;
+    private Button btnSelectLiquor;
+    private Button btnSelectElse;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_before_connect_select);
+
+        btnSelectSoju = (Button) findViewById(R.id.btn_sel_soju);
+        btnSelectBeer = (Button) findViewById(R.id.btn_sel_beer);
+        btnSelectWine = (Button) findViewById(R.id.btn_sel_wine);
+        btnSelectMakkulli = (Button) findViewById(R.id.btn_sel_makkulli);
+        btnSelectLiquor = (Button) findViewById(R.id.btn_sel_liquor);
+        btnSelectElse = (Button) findViewById(R.id.btn_sel_else);
+
+        // Button 클릭(기타 제외) -> SharedPreference 저장 후 기기 연결 페이지 이동
+        // 이거 안 되는데 수정하세요! 수정하고 블로그 올리세요!!
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SharedPreference 변수 선언
+                switch (v.getId()){
+                    case R.id.btn_sel_soju:
+                        Toast.makeText(getApplicationContext(), "소주 선택!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.btn_sel_beer:
+                        Toast.makeText(getApplicationContext(), "맥주 선택!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.btn_sel_wine:
+                        Toast.makeText(getApplicationContext(), "와인 선택!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.btn_sel_makkulli:
+                        Toast.makeText(getApplicationContext(), "막걸리 선택!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.btn_sel_liquor:
+                        Toast.makeText(getApplicationContext(), "양주 선택!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                // 여기에 기기 연결 페이지 이동문 작성하기.
+                Toast.makeText(getApplicationContext(), "기기 연결로 갑시다!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // 기타 Button 클릭 -> 술 정보 입력 팝업 생성 후 입력하면 기기 연결 페이지 이동
+        btnSelectElse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "팝업 입력 후, 기기 연결로 갑시다!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ Android/app/src/main/java/com/example/drinkingscanner/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.drinkingscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+// 서비스 선택 화면이 main 화면입니다:D
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnMoveConnect;
+    private Button btnMoveReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveConnect = (Button) findViewById(R.id.btn_move_connect_page);
+        btnMoveReport = (Button) findViewById(R.id.btn_move_report_page);
+
+        // 의문점 : 해당 페이지로 갔다가 뒤로 넘겼을 때 해당 페이지로 다시 넘어오는 거 어떻게 구현하지?
+        // 이거 그대로 생성하고 끝내 버리면 계속 화면 생성된 채로 있는 거니까 최적화가 안 되는 거 아님?
+        // -> 일단 구현부터 하고 생각하겠음.
+        // Button 클릭 -> 새 액티비티(ConnectActivity) 생성
+        btnMoveConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BeforeConnectSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button 클릭 -> 새 액티비티(ReportActivity) 생성
+        btnMoveReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "당신의 음주 습관 액티비티를 띄웁니다.", Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
+                //tartActivity(intent);
+            }
+        });
+    }
+}
