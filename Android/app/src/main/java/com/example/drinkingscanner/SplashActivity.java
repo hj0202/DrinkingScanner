0daeff9d9b475f928da30c5260f503d185d41aea
nhee0410 <nhee0410@naver.com>
2021-09-05 18:40:04 +0900

    [Android] Feat : Splash 화면 구현

---
+++ Android/app/src/main/java/com/example/drinkingscanner/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.drinkingscanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
