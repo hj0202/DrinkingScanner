0503cc14c4a6a9766fa2dc02a13cd62542c5b272
hj0202 <49390080+hj0202@users.noreply.github.com>
2021-09-05 16:15:08 +0900

    [Android] Feat : 기기 연결된 화면 생성

---
+++ Android/app/src/main/java/com/example/drinkingscanner/DrinkingAlcohol.java
@@ -0,0 +1,14 @@
+package com.example.drinkingscanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrinkingAlcohol extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinking_alcohol);
+    }
+}
