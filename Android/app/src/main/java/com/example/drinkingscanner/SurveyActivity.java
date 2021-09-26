758e7caa4152155e73064b66cd696fc9ff5dacfc
hj0202 <49390080+hj0202@users.noreply.github.com>
2021-09-26 11:19:32 +0900

    [Android] Feat : 설문조사화면에 '누구랑' 입력 방식 변경

--- Android/app/src/main/java/com/example/drinkingscanner/SurveyActivity.java
+++ Android/app/src/main/java/com/example/drinkingscanner/SurveyActivity.java
@@ -1,5 +1,6 @@
 package com.example.drinkingscanner;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,16 +19,19 @@ public class SurveyActivity extends AppCompatActivity {
     RadioGroup satisfyRadio;
     RadioGroup drunkRadio;
     EditText moneyInput;
-    EditText personInput;
+    RadioGroup whoRadio;
     Button saveButton;
+    TextView alcoholText;
+    TextView amountText;
 
-    // 보내야 하는 데이터
+    // 데이터 친구들
     String user;            // 사용자
     int drunk;              // 취기 정도
     int satisfy;            // 만족도
     String alcohol;         // 술 종류
-    String person;          // 누구랑
+    String who;             // 누구랑
     int money;              // 돈
+    int amount;             // 마신 양
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,9 +42,12 @@ public class SurveyActivity extends AppCompatActivity {
         satisfyRadio = findViewById(R.id.satisfyRadio);
         drunkRadio = findViewById(R.id.drunkRadio);
         moneyInput = findViewById(R.id.moneyInput);
-        personInput = findViewById(R.id.personInput);
+        whoRadio = findViewById(R.id.whoRadio);
         saveButton = findViewById(R.id.saveButton);
+        alcoholText = findViewById(R.id.alcoholText);
+        amountText = findViewById(R.id.amountText);
 
+        // 저장하기 버튼 클릭
         View.OnClickListener clickSaveButton = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,28 +57,32 @@ public class SurveyActivity extends AppCompatActivity {
                 int drunkId = drunkRadio.getCheckedRadioButtonId();
                 RadioButton drunkResult = findViewById(drunkId);
 
+                int whoId = whoRadio.getCheckedRadioButtonId();
+                RadioButton whoResult = findViewById(whoId);
+
                 satisfy = Integer.parseInt(satisfyResult.getText().toString());
                 drunk = Integer.parseInt(drunkResult.getText().toString());
                 money = Integer.parseInt(moneyInput.getText().toString());
-                person = personInput.getText().toString();
+                who = whoResult.getText().toString();
 
                 // TODO: 아래 5개 데이터를 데이터베이스에 정보 보내기 
                 Log.d("Survey", "send 취기 정도(int): " + drunk);
-                Log.d("Survey", "send 만족도(int): " + satisfy);
+                Log.d("Survey", "send 만족도(int): "    + satisfy);
                 Log.d("Survey", "send 술종류(string): " + alcohol);
-                Log.d("Survey", "send 누구랑(string): " + person);
-                Log.d("Survey", "send 돈(int): " + money);
+                Log.d("Survey", "send 누구랑(string): " + who);
+                Log.d("Survey", "send 돈(int): "       + money);
             }
         };
-
         saveButton.setOnClickListener(clickSaveButton);
 
-        // TODO: 유저 이름 받아오기
-        user = "현지";                // 임시 값
+        // TODO: 캘린더에서 마신 양 받아오기
+        amount = 300;         // 임시 값
 
-        // TODO: 캘린더에서 마신 양, 주종 받아오기
-        int allAmount = 300;         // 임시 값
-        alcohol = "소주";             // 임시 값
+        SharedPreferences sharedPreferences = getSharedPreferences("file", MODE_PRIVATE);
+        alcohol = sharedPreferences.getString("주종","");
+
+        amountText.setText(amount + "ml");
+        alcoholText.setText(alcohol);
     }
 }
 
