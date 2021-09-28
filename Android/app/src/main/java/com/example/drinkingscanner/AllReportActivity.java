f753d03b30d9c6ff1d467f05b0bb37381ed7093f
hj0202 <49390080+hj0202@users.noreply.github.com>
2021-09-29 06:11:20 +0900

    [Android] Feat : 누적 통계 화면 추가

---
+++ Android/app/src/main/java/com/example/drinkingscanner/AllReportActivity.java
@@ -0,0 +1,15 @@
+package com.example.drinkingscanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_report);
+    }
+}
